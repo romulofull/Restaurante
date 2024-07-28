95962f7f3528dd34ac88223f60c6186282f48d09
romulofull <romulo.castro@gmail.com>
2024-07-28 11:35:38 -0500

    Restaurante Total

---
+++ js/ejercicio restaurante.js
@@ -0,0 +1,7 @@
+var totalcomida = 2800 + 1300;
+var totalbebida= 900 + 650;
+var Total = totalcomida + totalbebida;
+
+console.log("El total de la comida es " + totalcomida);
+console.log("El total de la bebida es " + totalbebida);
+console.log("El total de la Cuenta es  " + Total);
